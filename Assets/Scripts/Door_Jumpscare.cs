bc02ced843fed667b1d16775c478becb6d95d0a8
FrancuzA <149786616+FrancuzA@users.noreply.github.com>
2025-05-07 11:04:00 +0200

    o diable

--- Assets/Scripts/Door_Jumpscare.cs
+++ Assets/Scripts/Door_Jumpscare.cs
@@ -2,9 +2,23 @@ using UnityEngine;
 
 public class Door_Jumpscare : MonoBehaviour
 {
+    public Animator DoorAnim;
     private void Awake()
     {
         float RNG = Random.Range(0f, 10f);
         if (RNG <= 3f) { Debug.Log("BOOOO"); };
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            float RNG = Random.Range(0f, 10f);
+            if (RNG <= 3f) 
+            { 
+             Debug.Log("BOOOO");
+             DoorAnim.SetBool("JumpScare", true);
+            }
+        }
     }
 }
